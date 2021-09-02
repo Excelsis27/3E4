3de4cb8d9421323c0ec49ea5945d98fff62b9bc7
André Lapointe <andre.lapointe@outlook.com>
2021-09-02 16:44:23 -0400

    cours04

--- S02/src/app.js
+++ S02/src/app.js
@@ -20,16 +20,54 @@ app.get('/date', (req, res) => {
 
 });
 
-app.get('/somme', (req, res) => {
+//maths/somme
+//maths/difference
+//maths/produit
+//maths/quotient
+//maths/reste
 
-    //console.log(req.query);
+app.get('/maths/:operation', (req, res) => {
+
+    const operation = req.params.operation;
+    console.log(operation);
+
 
     const a = parseInt(req.query.a, 10);
     const b = parseInt(req.query.b, 10);
 
+
+    let result = 0;
+    switch(operation)
+    {
+        case 'somme':
+            result = a + b;
+        break;
+        case 'produit':
+            result = a * b;
+        break;
+        case 'difference':
+            result = a - b;
+        break;
+        case 'quotient':
+            result = a / b;
+        break;
+        case 'reste':
+            result = a % b;
+        break;
+        default:
+            //res.status(400);
+            return res.status(400).end();
+            //return res.send('Opération non reconnue');
+            //return res.end();
+    }
+
     res.status(200);
     res.set('Content-Type', 'text/html');
-    res.send(`${a+b}`);
+    res.send(`${result}`);
+
+    //console.log(req.query);
+
+
 })
 
 export default app;
