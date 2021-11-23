fd870718a47237f465f6bb278815bd1cb770f655
André Lapointe <andre.lapointe@outlook.com>
2021-11-23 05:52:29 -0500

    Final commit - class theory completed

---
+++ S11/src/repositories/explorations.repository.js
@@ -0,0 +1,46 @@
+import e from 'express';
+import Exploration from '../models/exploration.model.js';
+import planetRepository from './planet.repository.js';
+
+class ExplorationsRepository {
+    
+    retrieveAll(retrieveOptions) {
+        const retrieveQuery = Exploration.find().skip(retrieveOptions.skip).limit(retrieveOptions.limit);
+        const countQuery = Exploration.estimatedDocumentCount();
+
+        return Promise.all([retrieveQuery, countQuery]);
+    }
+
+    retrieveById(idExploration, retrieveOptions) {
+        const retrieveQuery = Exploration.findById(idExploration);
+
+        if (retrieveOptions.planet) {
+            retrieveQuery.populate('planet');
+        }
+
+        return retrieveQuery;
+    }
+
+    transform(exploration, transformOptions = {}) {
+
+        if (transformOptions.embed && transformOptions.embed.planet) {
+            // ?embed=planet
+            // exploration.planet est un objet complet
+            exploration.planet = planetRepository.transform(exploration.planet, transformOptions);
+        } else {
+            // On veut pas embed la planet
+            // exploration.planet => seulement le id
+            exploration.planet = { href: `/planets/${exploration.planet}`};
+        }
+
+        // Choix pour la SERVER_URL
+        //exploration.href = `${process.env.SERVER_URL}/explorations/${exploration._id}`;
+        exploration.href = `/explorations/${exploration._id}`;
+
+        delete exploration._id;
+
+        return exploration;
+    }
+}
+
+export default new ExplorationsRepository();
